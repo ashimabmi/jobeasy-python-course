b9b5ac3c611b01a628b27497bc7d88a902580635
ashimabmi <ashimabmi@gmail.com>
2020-12-09 17:14:49 -0800

    this is home work2

--- lesson_1/homework_1_2.py
+++ lesson_1/homework_1_2.py
@@ -1,25 +1,29 @@
 # Find the second power of a variable. Save the expression to result_1 variable
 
-a = 10
-result_1 = None
+a: int = 10
+result_1 = pow(a, 2)
+print(result_1)
 
 
 # Convert integer variable b to a float. Save the expression to result_2 variable
 
-b = 10
-result_2 = None
+b: int = 10
+result_2 = float(b)
+print(result_2)
 
 
 # Convert a float variable c to integer. Save the expression to result_3 variable
 
-c = 5.04
-result_3 = None
+c: float = 5.04
+result_3 = int(c)
+print(result_3)
 
 
 # Sum up variables d and e and then multiply the total by f. Convert result to an integer and save the
 # expression to result_4 variable
-
 d = 5.04
 e = 2
 f = 1.2
-result_4 = None
+result = (d+e)*f
+result_4 = int(result)
+print(result_4)
